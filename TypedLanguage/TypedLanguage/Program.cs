bda3a079ec72baecba55c03ca23da13294720193
delysha <34533@ma-web.nl>
2023-02-23 00:36:24 +0100

    TypedLanguage

---
+++ TypedLanguage/TypedLanguage/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace TypedLanguage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int leeftijd = 9;
+            bool finished = true;
+
+            float x = 0.1f;
+            bool gameName = true;
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    internal class GameEngine
+    {
+        GameEngineState state;
+        
+        internal bool load (string file)
+        {
+            return true;
+        }
+        internal void plus(int x,int y) 
+        { 
+
+        }
+    }
+    internal class GameEngineState
+    {
+    }
+    internal interface IGameEngine
+    {
+    }
+
+}
