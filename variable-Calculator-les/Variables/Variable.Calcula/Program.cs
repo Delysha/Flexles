8abe66ce8fdf9576c6788ee66133216ae0fd7451
delysha <34533@ma-web.nl>
2023-03-03 12:41:54 +0100

    Variables and Calculate

---
+++ variable-Calculator-les/Variables/Variable.Calcula/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Variable.Calculate
+{
+    internal class Program
+    {
+        //class variable
+        string programTitle = "variable demo";
+        int programeWindowsPosx = 100;
+        static void Main(string[] args)
+        {
+            //type Name 
+
+
+            //local variables 
+            int xPos = 100;
+
+            xPos++;
+            xPos--;
+            xPos += 3;
+            xPos = xPos + 10 + 100 + 29;
+
+            xPos = (10 + 9)* 8;
+
+
+            Console.WriteLine(xPos);
+        }
+    }
+}
