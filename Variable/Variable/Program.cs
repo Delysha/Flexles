74754cdfbb7ce7ba7abb46834e9331bd43142541
delysha <34533@ma-web.nl>
2023-03-03 12:23:56 +0100

    Variables

---
+++ Variable/Variable/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Variable
+{
+    internal class Program
+    {
+        //class variable
+        string programTitle = "variable demo";
+        int programeWindowsPosx = 100;
+        static void Main(string[] args)
+        {
+            int xPos= 100;
+            float yPos;//0 Default value
+
+            yPos= 0.5f;
+
+            double bedrag;
+            string playerName;//null
+            
+            playerName = "teacher123";
+            bool isAfgestudeerd;
+
+            playerName = "student123";
+            Console.WriteLine("yPos"); 
+        }
+    }
+}
