04fc2f04909a6286d51f0e936326b1023a285a3b
delysha <34533@ma-web.nl>
2023-04-20 11:51:16 +0200

    Del

---
+++ functions-extra/functions-extra/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace functions_extra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program main = new Program();
+            Console.WriteLine("Hello, World!");
+            main.IntFunction(9);
+            main.StrFunction("Hello World, but with a function");
+        }
+        void IntFunction(int numb)
+        {
+            Console.WriteLine(numb);
+        }
+        void StrFunction(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
