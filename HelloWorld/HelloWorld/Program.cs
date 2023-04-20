d2f2446827bb6850c093c149e976761a7adaf8e9
delysha <34533@ma-web.nl>
2023-04-20 11:05:47 +0200

    HelloWorld

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "hello world!";
+            {
+                foreach (char c in message)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(c);
+                    Console.ResetColor();
+
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+
+        }
+    }
+}
