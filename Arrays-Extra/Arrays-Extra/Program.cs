7521b39fc36f7679a302589f2603e6661ba1ea1c
delysha <34533@ma-web.nl>
2023-04-20 11:25:04 +0200

    ArrayExtra

---
+++ Arrays-Extra/Arrays-Extra/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Arrays_Extra
+{
+    internal class Program
+    {
+
+        int[] nuts = new int[] { 10, 20, 30, 40, 50, 60 };
+        static void Main(string[] args)
+        {
+            Program main = new Program();
+            int ArrayTotal = main.ArrayFunction(main.nuts);
+            Console.WriteLine(ArrayTotal);
+            Console.WriteLine("Hello, World!");
+        }
+        internal int ArrayFunction(int[] array)
+        {
+            int total = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                total += array[i];
+            }
+            return total;
+        }
+    }
+
+}
