15f694640ff5a552636eaec9737c54f3a241adf3
delysha <34533@ma-web.nl>
2023-04-20 11:43:28 +0200

    Classes-extra

---
+++ Classes-Extra/Classes-Extra/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Classes_Extra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Rectangle rect = new Rectangle();
+
+            rect.x = 100;
+            rect.y = 200;
+            rect.width = 50;
+            rect.height = 50;
+
+
+        }
+       
+    }
+}
---
+++ Classes-Extra/Classes-Extra/Rectangle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Extra
+{
+    internal class Rectangle
+    {
+        public int x, y, width, height;
+    }
+}
